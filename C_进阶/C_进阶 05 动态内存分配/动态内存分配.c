1c6fe6e1216270172f2d88c393c33181f3133468
zhengyiq <601666966@qq.com>
2022-11-04 07:52:46 +0800

    动态内存分配代码

--- C_进阶/C_进阶 05 动态内存分配/动态内存分配.c
+++ C_进阶/C_进阶 05 动态内存分配/动态内存分配.c
@@ -91,41 +91,62 @@
 //}
 ////内存泄漏
 
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int* p = (int*)calloc(10, sizeof(int));
+//	if (p == NULL)
+//	{
+//		perror("calloc");
+//		return 1;
+//	}
+//	//增加空间
+//	int* ptr = (int*)realloc(p, sizeof(int)*20);//调整之后新的大小
+//	//当realloc开辟失败的是，返回的是NULL
+//
+//	if (ptr != NULL)
+//	{
+//		p = ptr;
+//		ptr = NULL;
+//	}
+//	//这一块需要特别的注意我们不可以将之前空间开辟的p指针直接指向我们使用realloc开辟的空间，如果直接将p指针指向realloc的空间，如果空间开辟失败那么我们原先数据就会丢失。
+//
+//
+//	for (int i = 10; i < 20; i++)
+//	{
+//		*(p + i) = i;
+//	}
+//
+//	//释放
+//	free(p);
+//	p = NULL;
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int*p = (int*)realloc(NULL, 40);//等价于malloc(40);
+//	return 0;
+//}
+
 #include <stdio.h>
 
-int main()
+int globalVal = 1;
+static int staticGlobalVar = 1;
+
+void Test()
 {
-	int* p = (int*)calloc(10, sizeof(int));
-	if (p == NULL)
-	{
-		perror("calloc");
-		return 1;
-	}
-	//增加空间
-	int* ptr = (int*)realloc(p, sizeof(int)*20);//调整之后新的大小
-	//当realloc开辟失败的是，返回的是NULL
+	static int staticVar = 1;
+	int localVar = 1;
+	int num1 = 10;
+	char arr[] = "abcd";
+	char* pc = NULL;
+	int* ptr1 = (int*)malloc(sizeof(int));
+	int* ptr2 = (int*)calloc(4,sizeof(int));
+	int* ptr3 = (int*)realloc(ptr2,sizeof(int));
 
-	if (ptr != NULL)
-	{
-		p = ptr;
-		ptr = NULL;
-	}
-	//这一块需要特别的注意我们不可以将之前空间开辟的p指针直接指向我们使用realloc开辟的空间，如果直接将p指针指向realloc的空间，如果空间开辟失败那么我们原先数据就会丢失。
-
-
-	for (int i = 10; i < 20; i++)
-	{
-		*(p + i) = i;
-	}
-
-	//释放
-	free(p);
-	p = NULL;
-	return 0;
-}
-
-int main()
-{
-	int*p = (int*)realloc(NULL, 40);//等价于malloc(40);
-	return 0;
+	free(ptr1);
+	free(ptr2);
+	free(ptr3);
 }
