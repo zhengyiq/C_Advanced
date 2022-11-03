9769d43b2460e01528258d70881b45f3b200b611
zhengyiq <601666966@qq.com>
2022-11-03 09:36:26 +0800

    文件操作

--- C_进阶/文件操作/文件操作.c
+++ C_进阶/文件操作/文件操作.c
@@ -1,43 +1,43 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-
-#include <stdio.h>
-
-int main(void)
-{
-	//打开文件
-	//相对路径
-	//FILE* pf = fopen("F:\\code\\text.txt", "w");
-	//绝对路径
-	FILE* pf = fopen("text.txt","r");
-	//判断是否读写成功
-	if (NULL == pf)
-	{
-		perror("fopen");
-	}
-
-
-	//写文件
-	//for (int i = 0; i < 26; i++)
-	//{
-	//	fputc('a'+ i,pf);
-	//}
-	//读文件
-	//for (int i = 0; i < 26; i++)
-	//{
-	//	int ch = fgetc(pf);
-	//	printf("%c ", ch);
-	//}
-	int ch = 0;
-
-	while ((ch = fgetc(pf)) != EOF)
-	{
-		printf("%c ", ch);
-	}
-
-
-	
-	//关闭文件
-	fclose(pf);
-	pf = NULL;
-	return 0;
-}
+//
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	//打开文件
+//	//相对路径
+//	//FILE* pf = fopen("F:\\code\\text.txt", "w");
+//	//绝对路径
+//	FILE* pf = fopen("text.txt","r");
+//	//判断是否读写成功
+//	if (NULL == pf)
+//	{
+//		perror("fopen");
+//	}
+//
+//
+//	//写文件
+//	//for (int i = 0; i < 26; i++)
+//	//{
+//	//	fputc('a'+ i,pf);
+//	//}
+//	//读文件
+//	//for (int i = 0; i < 26; i++)
+//	//{
+//	//	int ch = fgetc(pf);
+//	//	printf("%c ", ch);
+//	//}
+//	int ch = 0;
+//
+//	while ((ch = fgetc(pf)) != EOF)
+//	{
+//		printf("%c ", ch);
+//	}
+//
+//
+//	
+//	//关闭文件
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
